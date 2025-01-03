53e0fa3c7390aef496cd01fd2623769ab772d6cf
MT0220 <mt0220min@proton.me>
2025-01-03 17:59:11 +0000

    commit

--- fade.js
+++ fade.js
@@ -1,17 +1,17 @@
 // Remove the transition class
-const square1 = document.querySelector('.square1');
-square1.classList.remove('square1-transition');
+const header__container = document.querySelector('.header__container');
+header__container.classList.remove('square1-transition');
 
 // Create the observer, same as before:
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      square1.classList.add('square1-transition');
+      header__container.classList.add('square1-transition');
       return;
     }
 
-    square1.classList.remove('square1-transition');
+    header__container.classList.remove('square1-transition');
   });
 });
 
-observer.observe(document.querySelector('.square1-wrapper'));
+observer.observe(document.querySelector('.header__body'));
