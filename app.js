fbaf1365f98674f5f7537f4ee861f2ab4420f929
MT0220 <mt0220min@proton.me>
2025-01-07 16:30:50 +0000

    COMMIT

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-/* || JAVA DROP DOWN MENU || */
+/* 
 
-/*
-    "const" a variable that does not change, eg 
-    cannot be redeclared or reassigned (constant)
+|| JAVA DROP DOWN MENU ||
 
-    "document.querySelector" is a command for 
-    javascript to find HTML by CSS selectors
+    || CONST ||
+    "const" a variable that does not change, eg cannot be redeclared or reassigned (constant)
 
-    
+    || QUERY SELECTER ||
+    "document.querySelector" is a command for javascript to find HTML by CSS selectors
+
+    || EVENT LISTENER ||
+    "addEventListener" is as it sounds, an event listener. it is followed in brackets by the 
+    event type it is waiting for (eg, click) then function and tge function name (if set up 
+    like below, a function name isnt needed as it is local (a function name could then be 
+    used global))
+
+    || CLASS LIST ||
+    "classList" returns the CSS class names of HTML elements, using additional selectors 
+    (eg .add / .remove / .toggle) you are able to manipulate the HTML side.
+
+    || TOGGLE ||
+    ".toggle" is an "classList" selector enabling the elements CSS class names to be changed
 
 */
+
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav__menu');
 
