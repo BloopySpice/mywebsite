630c78b96ff42313d69e4e3628171632853861b5
MT0220 <mt0220min@proton.me>
2025-01-07 16:13:09 +0000

    COMMIT

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+/* || JAVA DROP DOWN MENU || */
+
+/*
+    "const" a variable that does not change, eg 
+    cannot be redeclared or reassigned (constant)
+
+    "document.querySelector" is a command for 
+    javascript to find HTML by CSS selectors
+
+    
+
+*/
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav__menu');
 
